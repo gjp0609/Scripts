5972a0a87eceda6bcd6e2306f08e2159c67b9259
guojinpeng <gjp0609@qq.com>
2020-07-29 16:41:16 +0800

    edit sh

--- shell/rs.sh
+++ shell/rs.sh
@@ -1,7 +1,7 @@
-cd /my/tools/tools || exit
-git pull https://username:password@github.com/gjp0609/tools.git master
+cd ~/tools/tools || exit
+git pull https://github.com/gjp0609/tools.git master
 source /etc/profile.d/jdk11.sh
 mvn clean package -DskipTests -P server
 kill -9 $(ps -ef | grep tools | grep -v "grep" | awk '{print $2}')
-cd /my/tools || exit
+cd ~/tools || exit
 nohup java -jar tools/target/tools.jar &

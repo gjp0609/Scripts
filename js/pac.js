905c5391848744c2ecaa672a66de921289b0b0e3
guojinpeng <gjp0609@qq.com>
2020-11-23 14:34:00 +0800

    edit pac

--- js/pac.js
+++ js/pac.js
@@ -1,19 +1,41 @@
 // SwitchyOmega pac
-var rules = [
-    "||api.github.com",
-    "||github.io",
-    "||jquery.com",
-    "||google.com",
+let domains = [
+    "api.github.com", // github api
+    "githubusercontent.com", // github api
+    "github.io",
+    "jquery.com",
+];
+let regexps = [
+    /\.google$/,
+    /\.google\w*.com$/,
 ];
 
-var proxyAddress = "127.0.0.1:1080";
+let proxy = "127.0.0.1:1080";
 
-var Type = {
+let Type = {
     direct: "DIRECT",
-    proxy: "PROXY " + proxyAddress,
-    socks: "SOCKS " + proxyAddress
+    proxy: "PROXY " + proxy,
+    socks: "SOCKS " + proxy
 };
 
 function FindProxyForURL(url, host) {
-    return Type.proxy;
+    if (check(url, host)) {
+        return Type.proxy;
+    } else {
+        return Type.direct;
+    }
+}
+
+function check(url, host) {
+    for (let domain of domains) {
+        if (host.endsWith(domain)) {
+            return true;
+        }
+    }
+    for (let regexp of regexps) {
+        if (regexp.test(host)) {
+            return true;
+        }
+    }
+    return false;
 }
